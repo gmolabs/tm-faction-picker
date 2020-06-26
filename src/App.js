a73b70db4e828706d97ea2a0f2555dd7ea1f7c5f
Geoff Morris <geoffm@gmail.com>
2020-06-26 14:21:47 -0700

    Install tensorflowjs

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Terra Mystica Faction Picker</h1>
+        <form><button>Suggest Faction</button></form>
+        <div id="suggestion"></div>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +20,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
