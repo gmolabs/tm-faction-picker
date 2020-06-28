d89ebc6be0d296d39e21eddf9774d907a3fe907c
Geoff Morris <geoffm@gmail.com>
2020-06-27 21:26:43 -0700

    Add factions dict. Predict for all factions.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,68 @@ import * as tf from '@tensorflow/tfjs';
 
 function App() {
   const GAMESTATE_LENGTH = 119;
+  const COLORS = [
+    "black", "blue", "brown", "gray", "green", "red", "yellow"
+  ]
+  const FACTIONS =
+    [
+      {
+        faction: "Alchemists",
+        color: "black"
+      },
+      {
+        faction: "Auren",
+        color: "green"
+      },
+      {
+        faction: "Chaos Magicians",
+        color: "red"
+      },
+      {
+        faction: "Cultists",
+        color: "brown"
+      },
+      {
+        faction: "Darklings",
+        color: "black"
+      },
+      {
+        faction: "Dwarves",
+        color: "gray"
+      },
+      {
+        faction: "Engineers",
+        color: "gray"
+      },
+      {
+        faction: "Fakirs",
+        color: "yellow"
+      },
+      {
+        faction: "Giants",
+        color: "red"
+      },
+      {
+        faction: "Halflings",
+        color: "brown"
+      },
+      {
+        faction: "Mermaids",
+        color: "blue"
+      },
+      {
+        faction: "Nomads",
+        color: "yellow"
+      },
+      {
+        faction: "Swarmlings",
+        color: "blue"
+      },
+      {
+        faction: "Witches",
+        color: "green"
+      }
+    ]
   var gamestate;
   var encodedGamestate;
   var predictions = 0;
@@ -13,10 +75,19 @@ function App() {
     const model = await tf.loadLayersModel(
       './model/model.json');
     //model.summary();
-    gamestate = new Array(GAMESTATE_LENGTH).fill(0);
+
+    gamestate = [];
+    for (var i = 0; i < FACTIONS.length; i++) {
+      gamestate[i] = new Array(GAMESTATE_LENGTH).fill(0);
+      //encode faction to predict for
+      gamestate[i][98 + i] = 1;
+      //encode color for this faction
+      var myColor = FACTIONS[i].color;
+      var colorIndex = COLORS.indexOf(myColor);
+      gamestate[i][112 + colorIndex] = 1;
+    }
     encodedGamestate = tf.tensor(gamestate);
-    encodedGamestate = encodedGamestate.reshape([1, GAMESTATE_LENGTH]);
-    //console.log(encodedGamestate);
+
     const pred = tf.tidy(() => {
       const output = model.predict(encodedGamestate);
       predictions = Array.from(output.dataSync());
